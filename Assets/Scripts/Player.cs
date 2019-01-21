a5c807f7ee5d76d858360efc6e1f6b402021b31a
Erick Simões <erick.simoes@live.com>
2019-01-21 16:36:19 -0300

    Add horizontal movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    private float horizontalInput;
+    
+    void Start() {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+    }
+}
