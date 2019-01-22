e3b915cc76111f8116c5f4f946cfeb60ce86d183
Erick Simões <erick.simoes@live.com>
2019-01-22 10:59:41 -0300

    Add vertical movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,17 @@ public class Player : MonoBehaviour {
 
     public float speed;
     private float horizontalInput;
-    
+    private float verticalInput;
+
     void Start() {
         transform.position = new Vector3(0, 0, 0);
     }
 
     void Update() {
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
     }
 }
