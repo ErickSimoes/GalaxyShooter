09249a1cd6134ac69b0773caaa24c6b263de3399
Erick Simões <erick.simoes@live.com>
2019-03-04 14:26:55 -0300

    Add Enemy behaviour
    The enemy moves down and, if the positions in y is less than screen,
    it reappears in a random position on the top.

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour {
-    
-    void Start() {
+
+	[SerializeField]
+	private float speed = 0.8f;
+	private float xMax = 8f;
+	private float yMax = 6f;
+	private float xRandom;
+
+	void Start() {
         
     }
 
     void Update() {
-        
+
+		transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+		if (transform.position.y <= -yMax) {
+			xRandom = Random.Range(-xMax, xMax);
+			transform.position = new Vector3(xRandom, yMax);
+		}
+		
     }
 }
