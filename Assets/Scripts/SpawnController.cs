f840abf1075e9ef42a2a01c1794194fd9d5d96af
Erick Simões <erick.simoes@live.com>
2019-06-25 10:47:41 -0300

    Add SpawnController

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject enemyPrefab;
+	[SerializeField]
+	private GameObject[] powerupsPrefab;
+
+	private readonly float xMax = 8f;
+	private readonly float yPos = 6.25f;
+
+	void Start() {
+		InvokeRepeating("InstantiateEnemy", 1f, 2f);
+    }
+
+    void Update() {
+		
+    }
+
+	private void InstantiateEnemy() {
+		Vector3 randomPosition = new Vector3(Random.Range(xMax, -xMax), yPos);
+		Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+	}
+}
